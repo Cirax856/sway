0dc92047c19b9da5895851694f0015ef7664ac94
Cirax856 <Michal.Schneider07@seznam.cz>
2025-11-13 06:25:53 +0100

    Made selection for cmp on tab rather than enter

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -21,7 +21,7 @@ return {
 	    mapping = cmp.mapping.preset.insert({
 		['<C-n>'] = cmp.mapping.select_next_item(),
 		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<Tab>'] = cmp.mapping.confirm({ select = true }),
 		['<C-Space>'] = cmp.mapping.complete(),
         ['<C-d>'] = cmp.mapping.abort(),
 	    }),
